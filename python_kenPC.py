1317291d6059152cfc37b0725a1eb27601f76a4d
Murai821 <murasou.1002@gmail.com>
2025-04-21 15:38:16 +0900

    commit

--- python_kenPC.py
+++ python_kenPC.py
@@ -1,1 +1,4 @@
 print("Hello, World!!!!!!!!!!!!!")
+ #ランダムな数字を表示
+import random
+print(random.randint(1, 100))
--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 function calculateDaysBetweenDate(begin, end) {
+    const beginDate = new Date(begin);
+    const endDate = new Date(end);
 
+    // Calculate the difference in milliseconds
+    const differenceInTime = endDate.getTime() - beginDate.getTime();
+
+    // Calculate the difference in days
+    const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+
+    return differenceInDays;
 }
